290ac0752060477185095a279d99306ea914cd78
dan2-ux <danhlanhat9@gmail.com>
2025-03-12 14:11:46 +0700

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const PORT = 555;
 
 export const mongoDBURL = 
-    "mongodb+srv://danhlanhat9:Danhlanhat9&@bookstoremern.lezhv.mongodb.net/BookStoreMERN?retryWrites=true&w=majority&appName=BookStoreMERN"
+    "mongodb+srv://danhlanhat9:Danhlanhat9%26@bookstoremern.lezhv.mongodb.net/BookStoreMERN?retryWrites=true&w=majority&appName=BookStoreMERN";
